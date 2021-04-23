d7bec2a32ce0f90d57c32c7fb297e99ceefc8711
Yamin Himani <yaminhimani@outlook.com>
2021-04-23 15:20:48 -0400

    add urls

--- music_party/frontend/src/components/HomePage.js
+++ music_party/frontend/src/components/HomePage.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import RoomJoinPage from "./RoomJoinPage"
+import CreateRoomPage from "./CreateRoomPage"
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+
 
 
 export default class HomePage extends Component{
@@ -8,6 +12,14 @@ export default class HomePage extends Component{
 
 
   render(){
-    return <p> This is the home page</p>
+    return <Router>
+     <Switch>
+       <Route exact path='/'> <p> This is the HomePage</p></Route>
+       <Route path='/join' component={RoomJoinPage}> <p> This is the room join page</p></Route>
+       <Route path='/create' component={CreateRoomPage}> <p> This is the create page</p></Route>
+
+     </Switch>
+
+    </Router>;
   }
 }
