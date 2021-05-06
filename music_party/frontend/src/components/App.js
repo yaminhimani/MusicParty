64730ca9d4a006bd165930fa5cc4fbe4fb112c24
Yamin Himani <yaminhimani@outlook.com>
2021-05-06 15:58:04 -0400

    work on ui

--- music_party/frontend/src/components/App.js
+++ music_party/frontend/src/components/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import {render} from "react-dom"; 
 import HomePage from './HomePage';
-import RoomJoinPage from './RoomJoinPage'
-import CreateRoomPage from './CreateRoomPage'
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
 
 
 export default class App extends Component{
--- music_party/frontend/src/components/HomePage.js
+++ music_party/frontend/src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import RoomJoinPage from "./RoomJoinPage"
-import CreateRoomPage from "./CreateRoomPage"
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 
 
@@ -15,8 +15,8 @@ export default class HomePage extends Component{
     return <Router>
      <Switch>
        <Route exact path='/'> <p> This is the HomePage</p></Route>
-       <Route path='/join' component={RoomJoinPage}> <p> This is the room join page</p></Route>
-       <Route path='/create' component={CreateRoomPage}> <p> This is the create page</p></Route>
+       <Route path='/join' component={RoomJoinPage}></Route>
+       <Route path='/create' component={CreateRoomPage}></Route>
 
      </Switch>
 
